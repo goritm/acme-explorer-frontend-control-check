685adcb188ddb01ec357849c196d84c8df05e666
Jose Sandoval <joseasandovalp@gmail.com>
2022-04-04 15:20:43 +0200

    feat: finish login component

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -4,10 +4,26 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthService } from './services/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbFormFieldModule,
+  NbInputModule
+} from '@nebular/theme';
+import { AppRoutingModule } from '../routing/app-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbFormFieldModule,
+    NbInputModule,
+    NbButtonModule,
+    NbCardModule
+  ],
   providers: [AuthService],
   exports: [LoginComponent, RegisterComponent]
 })
