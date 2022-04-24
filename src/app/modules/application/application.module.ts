60b5da9817c4c6ff43ddd03ba68a6ac4a7370c38
Jose Sandoval <joseasandovalp@gmail.com>
2022-04-24 15:23:54 +0200

    feat: add basic application list

--- src/app/modules/application/application.module.ts
+++ src/app/modules/application/application.module.ts
@@ -1,10 +1,12 @@
+import { GraphQLModule } from './../graphql/graphql.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListApplicationsComponent } from './components/list-applications/list-applications.component';
+import { NbCardModule, NbListModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [ListApplicationsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, GraphQLModule, NbCardModule, NbListModule],
   exports: [ListApplicationsComponent]
 })
 export class ApplicationModule {}
--- src/app/modules/application/components/list-applications/list-applications.component.ts
+++ src/app/modules/application/components/list-applications/list-applications.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ListApplicationsService } from './list-applications.service';
+import { IApplication } from '../../interfaces/application.interface';
 
 @Component({
   selector: 'list-applications',
   templateUrl: './list-applications.component.html',
   styleUrls: ['./list-applications.component.scss']
 })
-export class ListApplicationsComponent implements OnInit {
-  constructor() {}
+export class ListApplicationsComponent {
+  applications: IApplication[] = [];
+  placeholders: any = [];
+  pageSize = 10;
+  pageToLoadNext = 1;
+  loading = false;
 
-  ngOnInit(): void {}
+  constructor(private listApplicationService: ListApplicationsService) {}
+
+  loadNext() {
+    if (this.loading) {
+      return;
+    }
+
+    this.loading = true;
+    this.placeholders = new Array(this.pageSize);
+    this.listApplicationService.fetch().subscribe(({ data }) => {
+      this.placeholders = [];
+      this.applications.push(...data.listApplications.data);
+      this.loading = false;
+      this.pageToLoadNext++;
+    });
+  }
 }
