08e8405da5aeeb627a461db7fe3f7e324f76917b
Jose Sandoval <joseasandovalp@gmail.com>
2022-04-24 11:43:56 +0200

    fix: ts errors in applications main component

--- src/app/components/applications/applications.component.ts
+++ src/app/components/applications/applications.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./applications.component.scss']
 })
 export class ApplicationsComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void {}
 }
