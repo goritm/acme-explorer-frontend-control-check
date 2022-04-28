09f860e312aceb2b536fef8aee908d27759e3df1
David Bujosa <davidbujosa@gmail.com>
2022-04-28 06:31:45 +0200

    refactor: move base and authentication into utils

---
+++ src/utils/classes/base.entity.ts
@@ -0,0 +1,19 @@
+import { IBaseEntity } from 'src/utils/interfaces/base-entity.interface';
+
+export class BaseEntity implements IBaseEntity {
+  private _id: string;
+  private _version: number;
+
+  constructor(id: string, version: number) {
+    this._id = id;
+    this._version = version;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get version(): number {
+    return this._version;
+  }
+}
