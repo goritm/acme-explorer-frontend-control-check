5091eb0927480fbc458998506ef456f0ba388b71
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-11 21:24:02 +0200

    feat: create upload image service

---
+++ src/app/components/upload/image.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  constructor(private http: HttpClient) {}
+
+  public uploadImage(image: File) {
+    const formData = new FormData();
+    formData.append('file', image);
+
+    return this.http.post(`${environment.apiUrl}/picture`, formData, {
+      headers: new HttpHeaders({
+        Authorization: localStorage.getItem('token')
+          ? `Bearer ${localStorage.getItem('token')}`
+          : ''
+      }),
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
+}
