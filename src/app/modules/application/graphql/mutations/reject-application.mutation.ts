5a32f82505faf0cbd245ec8d9845ebad16a72a69
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-15 11:24:41 +0200

    fix: fix reject application mutation

--- src/app/modules/application/graphql/mutations/reject-application.mutation.ts
+++ src/app/modules/application/graphql/mutations/reject-application.mutation.ts
@@ -2,8 +2,8 @@ import { gql } from 'apollo-angular';
 import { APPLICATION_DATA } from 'src/utils/fragments/fragments';
 
 export const REJECT_APPLICATION = gql`
-  mutation rejectApplication($id: ID!, $reasonRejected: String!) {
-    rejectApplication(id: $id, reasonRejected: $reasonRejected) {
+  mutation rejectApplication($input: RejectApplicationInput!) {
+    rejectApplication(input: $input) {
       ...ApplicationData
     }
   }
