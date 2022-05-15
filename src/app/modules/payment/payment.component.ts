0fd8fd20dbde2e8863e9be402d06878522149e01
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-15 19:00:36 +0200

    feat: add sponsorship payment

--- src/app/modules/payment/payment.component.ts
+++ src/app/modules/payment/payment.component.ts
@@ -12,7 +12,8 @@ import { PaymentService } from './payment.service';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  applicationId: string | undefined;
+  id: string | undefined;
+  type: 'application' | 'sponsorship' = 'application';
   price: number | undefined;
   public payPalConfig?: IPayPalConfig;
 
@@ -26,8 +27,15 @@ export class PaymentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      this.applicationId = params['applicationId'];
-      this.getApplicationData();
+      if (params['applicationId']) {
+        this.id = params['applicationId'];
+        this.type = 'application';
+        this.getApplicationData();
+      } else if (params['sponsorshipId']) {
+        this.id = params['sponsorshipId'];
+        this.type = 'sponsorship';
+        this.getSponsorshipData();
+      }
     });
 
     this.initConfig();
@@ -35,7 +43,15 @@ export class PaymentComponent implements OnInit {
 
   private getApplicationData(): void {
     this.applicationService
-      .getApplicationById(this.applicationId ?? '')
+      .getApplicationById(this.id ?? '')
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  private getSponsorshipData(): void {
+    this.applicationService
+      .getApplicationById(this.id ?? '')
       .subscribe((data) => {
         console.log(data);
       });
@@ -52,7 +68,7 @@ export class PaymentComponent implements OnInit {
             {
               amount: {
                 currency_code: 'EUR',
-                value: '9.99' // TODO: REPLACE WITH REAL VALUE
+                value: this.type === 'application' ? '9.99' : '10' // TODO: REPLACE WITH APPLICATION VALUE OR FLAT RATE
               }
             }
           ]
@@ -80,18 +96,33 @@ export class PaymentComponent implements OnInit {
           data
         );
 
-        this.paymentService.payApplication(this.applicationId ?? '').subscribe({
-          next: ({ data }) => {
-            if (!(data === undefined || data === null)) {
-              this.toastrService.success(`Thank you for your payment!.`);
-              this.router.navigate(['/applications']);
+        if (this.type === 'application') {
+          this.paymentService.payApplication(this.id ?? '').subscribe({
+            next: ({ data }) => {
+              if (!(data === undefined || data === null)) {
+                this.toastrService.success('Thank you for your payment!.');
+                this.router.navigate(['/applications']);
+              }
+            },
+            error: (err) => {
+              this.toastrService.danger(err.message);
+              console.error(err);
             }
-          },
-          error: (err) => {
-            this.toastrService.danger(err.message);
-            console.error(err);
-          }
-        });
+          });
+        } else if (this.type === 'sponsorship') {
+          this.paymentService.paySponsorship(this.id ?? '').subscribe({
+            next: ({ data }) => {
+              if (!(data === undefined || data === null)) {
+                this.toastrService.success('Thank you for your payment!.');
+                this.router.navigate(['/sponsors/self']);
+              }
+            },
+            error: (err) => {
+              this.toastrService.danger(err.message);
+              console.error(err);
+            }
+          });
+        }
       },
       onCancel: (data, actions) => {
         console.log('OnCancel', data, actions);
--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { PAY_APPLICATION } from './graphql/mutations/pay-application.mutation';
+import { PAY_SPONSORSHIP } from './graphql/mutations/pay-sponsorship.mutation';
 import { PayApplicationResponse } from './graphql/responses/pay-application.response';
+import { PaySponsorshipResponse } from './graphql/responses/pay-sponsorship.response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,15 @@ export class PaymentService {
   payApplication(id: string) {
     return this.apollo.mutate<PayApplicationResponse>({
       mutation: PAY_APPLICATION,
+      variables: {
+        id
+      }
+    });
+  }
+
+  paySponsorship(id: string) {
+    return this.apollo.mutate<PaySponsorshipResponse>({
+      mutation: PAY_SPONSORSHIP,
       variables: {
         id
       }
