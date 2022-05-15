1391cd444312c3a90a26604a22d9f5c450ef8ce3
David Bujosa <davidbujosa@gmail.com>
2022-05-15 12:56:13 +0200

    feat: implement configuration flat rate inputs and types

---
+++ src/app/modules/configuration/graphql/queries/list-configurations.query.ts
@@ -0,0 +1,13 @@
+import { gql } from 'apollo-angular';
+
+export const LIST_CONFIGURATIONS = gql`
+  query listConfigutations($start: Int, $limit: Int, $where: JSON) {
+    listConfigurations(input: { start: $start, limit: $limit, where: $where }) {
+      count
+      data {
+        id
+        flatRate
+      }
+    }
+  }
+`;
