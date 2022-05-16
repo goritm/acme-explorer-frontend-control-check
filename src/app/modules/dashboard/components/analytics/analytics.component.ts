a248413fee61ffb4c462123461b87581e71fa307
David Bujosa <davidbujosa@gmail.com>
2022-05-16 08:25:44 +0200

    fix: update charts

--- src/app/modules/dashboard/components/analytics/analytics.component.ts
+++ src/app/modules/dashboard/components/analytics/analytics.component.ts
@@ -1,4 +1,3 @@
-import { Router } from '@angular/router';
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
@@ -24,62 +23,10 @@ export class AnalyticsComponent implements OnInit {
   pricePerTripOptions: EChartsOption = {};
   applicationPerTripOptions: EChartsOption = {};
   tripManagedPerManagerOptions: EChartsOption = {};
-  ratioOfApplicationGroupByStateOptions: EChartsOption = {
-    tooltip: {
-      trigger: 'item'
-    },
-    visualMap: {
-      show: true,
-      min: 80,
-      max: 600,
-      inRange: {
-        colorLightness: [0, 1]
-      }
-    },
-    series: [
-      {
-        name: 'Access From',
-        type: 'pie',
-        radius: '55%',
-        center: ['50%', '50%'],
-        data: [
-          { value: 335, name: 'Direct' },
-          { value: 310, name: 'Email' },
-          { value: 274, name: 'Union Ads' },
-          { value: 235, name: 'Video Ads' },
-          { value: 400, name: 'Search Engine' }
-        ].sort(function (a, b) {
-          return a.value - b.value;
-        }),
-        roseType: 'radius',
-        label: {
-          color: 'rgba(255, 255, 255, 0.3)'
-        },
-        labelLine: {
-          lineStyle: {
-            color: 'rgba(255, 255, 255, 0.3)'
-          },
-          smooth: 0.2,
-          length: 10,
-          length2: 20
-        },
-        itemStyle: {
-          color: '#c23531',
-          shadowBlur: 200,
-          shadowColor: 'rgba(0, 0, 0, 0.5)'
-        },
-        animationType: 'scale',
-        animationEasing: 'elasticOut',
-        animationDelay: function () {
-          return Math.random() * 200;
-        }
-      }
-    ]
-  };
+  ratioOfApplicationGroupByStateOptions: EChartsOption = {};
 
   constructor(
     protected analyticsService: AnalyticsService,
-    private router: Router,
     private toastrService: NbToastrService,
     private cdr: ChangeDetectorRef
   ) {}
@@ -111,7 +58,11 @@ export class AnalyticsComponent implements OnInit {
                 this.analytics.pricePerTrip.max
               ],
               type: 'line',
-              areaStyle: {}
+              areaStyle: {
+                color: '#1A5276',
+                shadowBlur: 10,
+                origin: 'auto'
+              }
             }
           ]
         };
@@ -137,7 +88,11 @@ export class AnalyticsComponent implements OnInit {
                 this.analytics.applicationPerTrip.max
               ],
               type: 'line',
-              areaStyle: {}
+              areaStyle: {
+                color: '#3498DB',
+                shadowBlur: 10,
+                origin: 'auto'
+              }
             }
           ]
         };
@@ -163,7 +118,11 @@ export class AnalyticsComponent implements OnInit {
                 this.analytics.tripManagedPerManager.max
               ],
               type: 'line',
-              areaStyle: {}
+              areaStyle: {
+                color: '#212F3D',
+                shadowBlur: 10,
+                origin: 'auto'
+              }
             }
           ]
         };
