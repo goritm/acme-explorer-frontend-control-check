acca245a6797d6a0302f29990fb2e39ce799f34f
David Bujosa <davidbujosa@gmail.com>
2022-05-16 08:06:15 +0200

    feat: create analytics type

--- src/app/modules/dashboard/graphql/types/get-analytics-response.type.ts
+++ src/app/modules/dashboard/graphql/types/get-analytics-response.type.ts
@@ -1,13 +1,5 @@
-import { ApplicationPerTrip } from './application-per-trip.type';
-import { PricePerTrip } from './price-per-trip.type';
-import { RatioOfApplicationGroupByState } from './ratio-of-application-group-by-state.type';
-import { TripManagedPerManager } from './trip-managed-per-manager.type';
+import { Analytics } from './analytics.type';
 
 export type ResponseGetAnalyticsQuery = {
-  getAnalitycs: {
-    pricePerTrip: PricePerTrip;
-    applicationPerTrip: ApplicationPerTrip;
-    tripManagedPerManager: TripManagedPerManager;
-    ratioOfApplicationGroupByState: RatioOfApplicationGroupByState[];
-  };
+  getAnalitycs: Analytics;
 };
