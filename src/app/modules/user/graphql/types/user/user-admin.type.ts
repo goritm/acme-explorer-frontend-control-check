ac73b0caf7231f159ea61bc6290dbfff3cb49652
David Bujosa <davidbujosa@gmail.com>
2022-05-16 12:49:05 +0200

    fix: move user types into user root

--- src/app/modules/user/graphql/types/user/user-admin.type.ts
+++ src/app/modules/user/graphql/types/user/user-admin.type.ts
@@ -1,6 +1,6 @@
 import { UserRoles } from 'src/utils/enums/user-roles.enum';
 import { IBaseEntity } from 'src/utils/interfaces/base-entity.interface';
-import { UserStatus } from '../enum/user-status.enum';
+import { UserStatus } from '../../enum/user-status.enum';
 
 export type UserAdmin = IBaseEntity & {
   name: string;
