cede3c5519dc4372c1f548800f828cd74c14d114
David Bujosa <davidbujosa@gmail.com>
2022-05-16 21:02:41 +0200

    feat: init favorite list

---
+++ src/app/modules/favorite/components/list-favorite/favorite-list.component.ts
@@ -0,0 +1,68 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
+import { FavoriteService } from '../../services/favorite.service';
+import { FavoriteList } from '../../graphql/types/favorite-list.type';
+import { Trip } from 'src/app/modules/trip/graphql/types/trip.type';
+
+@Component({
+  selector: 'favorite-list',
+  templateUrl: './favorite-list.component.html',
+  styleUrls: ['./favorite-list.component.scss']
+})
+export class FavoriteListComponent implements OnInit {
+  favoriteList!: FavoriteList;
+  count = 0;
+  pageSize = 10;
+  pageToLoadNext = 1;
+  loading = false;
+
+  constructor(
+    protected favoriteService: FavoriteService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastrService: NbToastrService,
+    private dialogService: NbDialogService
+  ) {}
+
+  removeFavoriteTrip(trip: Trip) {
+    this.favoriteService
+      .deleteFavoriteTrip({
+        id: this.favoriteList.id,
+        trip: trip.id
+      })
+      .subscribe({
+        next: () => {
+          this.toastrService.success('Trip removed from favorite list');
+          this.favoriteList.trips = this.favoriteList.trips.filter(
+            (f) => f.id !== trip.id
+          );
+        }
+      });
+  }
+
+  loadNext() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) {
+      return;
+    }
+
+    if (this.loading) {
+      return;
+    }
+
+    this.loading = true;
+    this.favoriteService.getSelfFavoriteListById(id).subscribe({
+      next: ({ data }) => {
+        this.favoriteList = data.getSelfFavoriteListById;
+        this.loading = false;
+        this.pageToLoadNext++;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadNext();
+  }
+}
