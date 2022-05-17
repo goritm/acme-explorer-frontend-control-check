0873bddc27674af332daa7b7022c6f0d1dc781f8
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-17 10:47:24 +0200

    test: add login as manager test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,20 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+describe('Homepage Initial Tender', () => {
+  it('Visits the trips homepage', () => {
     cy.visit('/');
-    cy.contains('Welcome');
-    cy.contains('sandbox app is running!');
+    cy.contains('ACME Explorer');
   });
+
+  it('logins as a manager', () => {
+    cy.get('[data-testid="login-button"]').click();
+
+    cy.get('#email').type('test2@test.com');
+    cy.get('#password').type('123456');
+    cy.get('[data-testid="auth-login-button"]').click();
+    cy.contains('ACME Explorer');
+    cy.get('[data-testid="user-bar"]').contains('manager');
+    cy.visit('/trips/self');
+  });
+
+  // it('goes to the self trips page', () => {
+  // });
 });
