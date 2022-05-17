4822b126fff235ea31128ac75452403524cf384e
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-17 11:30:54 +0200

    test: finish edit e2e test

--- cypress/integration/edit-trip.ts
+++ cypress/integration/edit-trip.ts
@@ -1,4 +1,4 @@
-describe('Homepage Initial Tender', () => {
+describe('Edit Trip e2e', () => {
   const managerCreds = {
     email: 'test2@test.com',
     password: '123456'
@@ -18,8 +18,22 @@ describe('Homepage Initial Tender', () => {
     cy.contains('ACME Explorer');
     cy.byTestId('user-bar').contains('manager');
     cy.visit('/trips/self');
+    cy.saveLocalStorage();
   });
 
-  // it('goes to the self trips page', () => {
-  // });
+  it('edits first self trip', () => {
+    const titleText = 'hello from cypress :)';
+    cy.byTestId('self-trip-card').first().click();
+    cy.byTestId('edit-trip-button').click();
+
+    cy.restoreLocalStorage();
+    cy.get('#title').clear().type(titleText);
+    cy.byTestId('submit-edit-button').click();
+
+    cy.wait(1000);
+
+    cy.contains(titleText);
+    cy.visit('/trips/self');
+    cy.byTestId('self-trip-card').first().contains(titleText);
+  });
 });
