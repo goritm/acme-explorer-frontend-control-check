7352dfe9fb09e5bdbf0913bd487815ca4c70507e
Jose Sandoval <joseasandovalp@gmail.com>
2022-05-17 20:20:38 +0200

    feat: add stage edition

--- src/app/modules/trip/components/trip-detail/dialog/edit-trip-dialog/edit-trip-dialog.component.ts
+++ src/app/modules/trip/components/trip-detail/dialog/edit-trip-dialog/edit-trip-dialog.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NbDialogRef, NbToastrService } from '@nebular/theme';
 import { finalize } from 'rxjs';
+import { Stage } from 'src/app/modules/trip/graphql/types/stage.type';
 import { UpdateTripInput } from 'src/app/modules/trip/inputs/update-trip.input';
 import { ITrip } from 'src/app/modules/trip/interfaces/trip.interface';
 import { TripService } from 'src/app/modules/trip/trip.service';
@@ -18,10 +19,17 @@ export class EditTripDialogComponent implements OnInit {
   loading = false;
   isOwnTrip = false;
 
+  stages: Stage[] = [];
+
   updateTripForm = this.fb.group({
     title: ['', [Validators.required]],
     description: ['', [Validators.required]],
-    requirements: ['', [Validators.required]]
+    requirements: ['', [Validators.required]],
+    stages: this.fb.group({
+      title: [''],
+      description: [''],
+      price: ['']
+    })
   });
 
   constructor(
@@ -37,51 +45,78 @@ export class EditTripDialogComponent implements OnInit {
     this.initForm();
   }
 
-  updateTrip() {
-    this.submitted = true;
-    this.loading = true;
+  addStage() {
+    const title = this.updateTripForm.get('stages.title')?.value;
+    const description = this.updateTripForm.get('stages.description')?.value;
+    const price = this.updateTripForm.get('stages.price')?.value;
 
-    const stages = [
-      {
-        title: 'hola',
-        description: 'chao',
-        price: this.trip.price + 100
-      }
-    ];
-
-    const updateTripInput: UpdateTripInput = {
-      data: {
-        ...this.updateTripForm.value,
-        requirements: this.updateTripForm.value.requirements.split('\n'),
-        stages: stages
-      },
-      where: {
-        id: this.trip.id
-      }
-    };
-
-    this.tripService
-      .updateTrip(updateTripInput)
-      .pipe(
-        finalize(() => {
-          this.loading = false;
-          this.cdr.detectChanges();
-        })
-      )
-      .subscribe({
-        next: ({ data }) => {
-          if (!(data === undefined || data === null)) {
-            this.toastrService.success(
-              `Trip ${this.trip.title} modified successfully`
-            );
-            this.updateTripForm.reset();
-            this.dialogRef.close();
-          }
-        },
-        error: (err) => {
-          this.toastrService.danger(err.message);
-        }
+    if (title && description && price) {
+      this.stages.push({ title, description, price });
+      this.updateTripForm.get('stages.title')?.setValue('');
+      this.updateTripForm.get('stages.description')?.setValue('');
+      this.updateTripForm.get('stages.price')?.setValue('');
+    } else {
+      this.toastrService.show('Please fill all fields', 'Error', {
+        duration: 3000,
+        status: 'danger'
       });
+    }
+  }
+
+  deleteStage(stageToDelete: {
+    title: string;
+    description: string;
+    price: number;
+  }) {
+    this.stages = this.stages.filter(
+      (stage) => stage.title !== stageToDelete.title
+    );
+  }
+
+  updateTrip() {
+    if (this.stages.length > 0) {
+      this.submitted = true;
+      this.loading = true;
+
+      const updateTripInput: UpdateTripInput = {
+        data: {
+          ...this.updateTripForm.value,
+          requirements: this.updateTripForm.value.requirements.split('\n'),
+          stages: this.stages
+        },
+        where: {
+          id: this.trip.id
+        }
+      };
+
+      this.tripService
+        .updateTrip(updateTripInput)
+        .pipe(
+          finalize(() => {
+            this.loading = false;
+            this.cdr.detectChanges();
+          })
+        )
+        .subscribe({
+          next: ({ data }) => {
+            if (!(data === undefined || data === null)) {
+              this.toastrService.success(
+                `Trip ${this.trip.title} modified successfully`
+              );
+              this.updateTripForm.reset();
+              this.dialogRef.close();
+            }
+          },
+          error: (err) => {
+            this.toastrService.danger(err.message);
+          }
+        });
+    } else {
+      this.toastrService.show('Add at least one stage', 'Error', {
+        duration: 3000,
+        status: 'danger'
+      });
+    }
   }
 
   initForm() {
